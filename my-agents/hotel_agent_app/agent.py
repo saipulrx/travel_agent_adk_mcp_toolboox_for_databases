f0316fc3de03737f4c1031c011d9b984d6e232d0
Muhammad Rohman <msaipulrx@gmail.com>
2025-12-16 17:15:44 +0700

    add source code

---
+++ my-agents/hotel_agent_app/agent.py
@@ -0,0 +1,21 @@
+from google.adk.agents import Agent
+from toolbox_core import ToolboxSyncClient
+
+toolbox = ToolboxSyncClient("http://127.0.0.1:5000")
+#toolbox = ToolboxSyncClient("https://toolbox-487826826516.us-central1.run.app")
+
+# Load all the tools
+tools = toolbox.load_toolset('my_first_toolset')
+
+root_agent = Agent(
+    name="hotel_agent",
+    #model="gemini-2.5-flash",
+    model="gemini-3-pro-preview",
+    description=(
+        "Agent to answer questions about hotels in a city or hotels by name."
+    ),
+    instruction=(
+        "You are a helpful agent who can answer user questions about the hotels in a specific city or hotels by name. Use the tools to answer the question"
+    ),
+    tools=tools,
+)
